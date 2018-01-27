d9ccd02f3d2a002582a7687b1e6809b808c88b6d
mdsebald <mdsebald@gmail.com>
2018-01-26 20:35:01 -0600

    Learning Elm

--- elm/Main.elm
+++ elm/Main.elm
@@ -1,7 +1,49 @@
-module Main exposing (..)
+-- module Main exposing (..)
 
-import Html exposing (text)
+-- import Html exposing (text)
+
+
+-- main =
+--     text "Hello, World!"
+
+
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
 
 
 main =
-    text "Hello, World!"
+  Html.beginnerProgram { model = model, view = view, update = update }
+
+
+-- MODEL
+
+type alias Model = Int
+
+model : Model
+model =
+  0
+
+
+-- UPDATE
+
+type Msg = Increment | Decrement
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Increment ->
+      model + 1
+
+    Decrement ->
+      model - 1
+
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ button [ onClick Decrement ] [ text "-" ]
+    , div [] [ text (toString model) ]
+    , button [ onClick Increment ] [ text "+" ]
+    ]    
