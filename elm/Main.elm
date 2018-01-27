7c9959cb55f83217cb1b77838c4986c321157ae9
mdsebald <mdsebald@gmail.com>
2018-01-27 16:21:43 -0600

    Trying elm-markdown package

--- elm/Main.elm
+++ elm/Main.elm
@@ -7,43 +7,71 @@
 --     text "Hello, World!"
 
 
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+-- import Html exposing (Html, button, div, text)
+-- import Html.Events exposing (onClick)
+
+
+-- main =
+--   Html.beginnerProgram { model = model, view = view, update = update }
+
+
+-- -- MODEL
+
+-- type alias Model = Int
+
+-- model : Model
+-- model =
+--   0
+
+
+-- -- UPDATE
+
+-- type Msg = Increment | Decrement
+
+-- update : Msg -> Model -> Model
+-- update msg model =
+--   case msg of
+--     Increment ->
+--       model + 1
+
+--     Decrement ->
+--       model - 1
+
+
+-- -- VIEW
+
+-- view : Model -> Html Msg
+-- view model =
+--   div []
+--     [ button [ onClick Decrement ] [ text "-" ]
+--     , div [] [ text (toString model) ]
+--     , button [ onClick Increment ] [ text "+" ]
+--     ]    
+
+
+import Markdown
 
 
 main =
-  Html.beginnerProgram { model = model, view = view, update = update }
+  Markdown.toHtml [] markdown
 
 
--- MODEL
+markdown = """
 
-type alias Model = Int
+# LinkBlox - Linking blocks of functionality
 
-model : Model
-model =
-  0
+[Markdown](http://daringfireball.net/projects/markdown/) lets you
+write content in a really natural way.
 
+  * You can have lists, like this one
+  * Make things **bold** or *italic*
+  * Embed snippets of `code`
+  * Create [links](/)
+  * ...
 
--- UPDATE
+The [elm-markdown][] package parses all this content, allowing you
+to easily generate blocks of `Element` or `Html`.
 
-type Msg = Increment | Decrement
+[elm-markdown]: http://package.elm-lang.org/packages/evancz/elm-markdown/latest
 
-update : Msg -> Model -> Model
-update msg model =
-  case msg of
-    Increment ->
-      model + 1
-
-    Decrement ->
-      model - 1
-
-
--- VIEW
-
-view : Model -> Html Msg
-view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (toString model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    ]    
+"""
